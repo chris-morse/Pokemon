ea5afd3fce30da1606c9e6e59b5b3811ca1f8458
chris-morse <101614566+chris-morse@users.noreply.github.com>
2022-03-15 00:00:26 -0400

    Pokemon.hpp
    Defines class Pokemon

---
+++ Pokemon.hpp
@@ -0,0 +1,29 @@
+#ifndef POKEMON_HPP
+#define POKEMON_HPP
+
+  class Pokemon{
+      
+       private:
+  
+          
+        
+          
+          
+          
+          
+          
+       public:
+       
+          string name;
+          string nickname;
+       
+          int getHealth() const;
+  
+  };
+
+
+
+
+
+  
+#endif
